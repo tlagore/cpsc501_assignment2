4b58c439a4cb67c82b23e6812af56e02087d157e
Tyrone <Tyrone@192.168.0.102>
2016-10-25 22:14:39 -0600

    Wrote tests for getClassNameDetails()

--- tests/object_inspector_tests/Inspector_Tests.java
+++ tests/object_inspector_tests/Inspector_Tests.java
@@ -9,34 +9,34 @@ import object_inspector.Inspector;
 public class Inspector_Tests {
 
 	@Test
-	public void testGetClassName() {
+	public void testGetClassNameDetails() {
 		Inspector inspector = new Inspector();
 		
-		String[] name = inspector.getClassName(((Object)new String()).getClass());
+		String[] name = inspector.getClassNameDetails(((Object)new String()).getClass());
 		assertEquals(name[0],"0");
 		assertEquals(name[1],"java.lang.String");
 		
-		name = inspector.getClassName(((Object)true).getClass());
+		name = inspector.getClassNameDetails(((Object)true).getClass());
 		assertEquals(name[0],"0");
 		assertEquals(name[1],"java.lang.Boolean");
 		
-		name = inspector.getClassName(((Object)new String[2]).getClass());
+		name = inspector.getClassNameDetails(((Object)new String[2]).getClass());
 		assertEquals(name[0],"1");
 		assertEquals(name[1],"Ljava.lang.String");
 		
-		name = inspector.getClassName(((Object)(long)4).getClass());
+		name = inspector.getClassNameDetails(((Object)(long)4).getClass());
 		assertEquals(name[0],"0");
 		assertEquals(name[1],"java.lang.Long");
 
-		name = inspector.getClassName(((Object)new Float[1][1]).getClass());
+		name = inspector.getClassNameDetails(((Object)new Float[1][1]).getClass());
 		assertEquals(name[0],"2");
 		assertEquals(name[1],"Ljava.lang.Float");
 		
-		name = inspector.getClassName(((Object)new byte[1][1][1][1]).getClass());
+		name = inspector.getClassNameDetails(((Object)new byte[1][1][1][1]).getClass());
 		assertEquals(name[0],"4");
 		assertEquals(name[1],"B");
 		
-		name = inspector.getClassName(((Object)new char[1][1][1][1][1][1]).getClass());
+		name = inspector.getClassNameDetails(((Object)new char[1][1][1][1][1][1]).getClass());
 		assertEquals(name[0],"6");
 		assertEquals(name[1],"C");
 	}
